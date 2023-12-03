0ab9a697840b1c4255f77c14bedd015f8e3088ab
hstla <ree7243@gmail.com>
2023-12-03 16:27:03 +0900

    서버와 연결 테스트

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,29 @@
-export default function Home() {
+"use client";
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function Page() {
+  const [data, setData] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://backend:8080/api/test');
+      console.log(response.data);
+      setData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <main>
-      <h1>Threads</h1>
-    </main>
+    <div>
+      <h1>Test Page</h1>
+      <h2>{data ? JSON.stringify(data) : 'Loading data...'}</h2>
+    </div>
   );
 }
