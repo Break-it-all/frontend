1c2af856bb4f1d49782a0a3d7fac78ae67a31f62
최민우 <cmw0918@naver.com>
2023-12-26 10:59:55 +0900

    패키지 추가

--- src/components/MonacoEditorComponent.tsx
+++ src/components/MonacoEditorComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import Editor from "@monaco-editor/react";
 import * as Y from "yjs";
 import { WebsocketProvider } from "y-websocket";
